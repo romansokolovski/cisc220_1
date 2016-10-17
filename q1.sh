3030e1a371569102e02684de292ff79ce83f3f7c
fang <xufang5555@hotmail.com>
2016-10-17 13:43:27 -0400

    Question 1 final version

--- q1.sh
+++ q1.sh
@@ -1,28 +1,57 @@
-#!/bin/bash
+ #!/bin/bash
 echo Calculator:
 prog=1
-
-while (( $prog==1 ))
+re='^[0-9]+$'
+while (( $prog == 1 )) 
 do
 	echo Please insert a number: 
 	read num1
 	if [ "$num1" == ":q" ]; then
-		prog=0;
+		echo Thanks for using my calculator
+		break
 	fi
+	while ! [[ $num1 =~ $re ]]
+	do
+		echo Error! Please insert a number: 
+		read num1
+		if [ "$num1" == ":q" ]; then
+			echo Thanks for using my calculator
+			break
+		fi
+	done
+
 	while (( $prog==1 ))
 	do
 		echo Please insert an operation: 
 		read op
 		if [ "$op" == ":q" ]; then
-			prog=0;
-			break
+			echo Thanks for using my calculator
+			break 2
 		fi
+		while ! [[ "$op" == "+" || "$op" == "-" || "$op" == "/" || "$op" == "*" ]]
+		do
+			echo Error! Please insert an operation: 
+			read op
+			if [ "$op" == ":q" ]; then
+				echo Thanks for using my calculator
+				break 2
+			fi
+		done
 		echo Please insert a number:
 		read num2
 		if [ "$num2" == ":q" ]; then
-			prog=0;
-			break
+			echo Thanks for using my calculator
+			break 2
 		fi
+		while ! [[ $num2 =~ $re ]]
+		do
+			echo Error! Please insert a number:
+			read num2
+			if [ "$num2" == ":q" ]; then
+				echo Thanks for using my calculator
+				break 3
+			fi
+		done
 		if [ "$op" == "+" ]; then
 			counter=`expr $num1 + $num2`;
 		elif [ "$op" == "-" ]; then
