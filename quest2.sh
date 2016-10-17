354b8a2ed2388be822a79bcda51faa04aa103485
brandon <14blw@queensu.ca>
2016-10-16 22:32:18 -0400

    Question 2 Assign1

--- quest2.sh
+++ quest2.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 ip=$(/sbin/ifconfig lo | grep 'inet addr:' | cut -d: -f2 | awk '{print $1}')
-finalip=""
-if[$ip == 127*]; then
-	finalip =$ip"Local IP"
+finalip={$ip:0:3}
 
-elif [$ip == 10* || $ip == 192.168*]; then
-	finalip =$ip"Private IP"
+if[[$finalip == 127]]; then
+	finalip =$ip"Local IP";
+
+elif [[$ip == 10* || $ip == 192.168*]]; then
+	finalip =$ip"Private IP";
 
 else
-	finalip =$ip"Public IP"
+	finalip =$ip"Public IP";
 fi
-echo finalip
+
+echo $finalip
