f64a90dfdfed8603a0de5056780e1f7ff7e047e6
fang <xufang5555@hotmail.com>
2016-10-17 12:05:18 -0400

    mostly finished

--- q1.sh
+++ q1.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 echo Calculator:
 prog=1
-echo test
+
 while (( $prog==1 ))
 do
 	echo Please insert a number: 
 	read num1
-	echo Please insert an operation: 
-	read operation
-	echo Please insert a number:
-	read num2
-	if [[ $operation=="+" ]] ; then
-	counter=`expr $num1 + $num2`
-	else
-	echo This is else
+	if [ "$num1" == ":q" ]; then
+		prog=0;
 	fi
-	echo Result so far is: $counter
-	prog=1
+	while (( $prog==1 ))
+	do
+		echo Please insert an operation: 
+		read op
+		if [ "$op" == ":q" ]; then
+			prog=0;
+			break
+		fi
+		echo Please insert a number:
+		read num2
+		if [ "$num2" == ":q" ]; then
+			prog=0;
+			break
+		fi
+		if [ "$op" == "+" ]; then
+			counter=`expr $num1 + $num2`;
+		elif [ "$op" == "-" ]; then
+			counter=`expr $num1 - $num2`;
+		elif [ "$op" == "*" ]; then
+			counter=`expr $num1 \* $num2`;
+		elif [ "$op" == "/" ]; then
+			counter=`expr $num1 / $num2`;
+		else
+			echo This is else;
+		fi
+		num1=$counter
+		echo Result so far is: $counter
+	done
 done
