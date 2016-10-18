a222cb66861a918d1f9da0107fdc7e4faf5e3a71
brandon <14blw@queensu.ca>
2016-10-17 22:12:34 -0400

    question2

--- quest2.sh
+++ quest2.sh
@@ -2,12 +2,13 @@
 ip=$(/sbin/ifconfig lo | grep 'inet addr:' | cut -d: -f2 | awk '{print $1}')
 
 finalip=""
-if [[ $ip == "127*" ]] ; then
+if [ $ip == "127*" ] ; then
 	$ip+= "Local IP";
+fi
 
-elif [[ $ip == "10*" || $ip == "192.168*" ]]; then
+elif [ $ip == "10*" || $ip == "192.168*" ]; then
 	$ip+="Private IP";
-
+fi
 else
 	$ip+="Public IP";
 fi
